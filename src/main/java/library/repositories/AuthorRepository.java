e812642be68ee1c7867d9a056a83e70f9de11f63
Damian Grochowski <smg7@o2.pl>
2019-04-26 20:09:24 +0200

    first commit

--- src/main/java/library/repositories/AuthorRepository.java
+++ src/main/java/library/repositories/AuthorRepository.java
@@ -2,6 +2,14 @@ package library.repositories;
 
 import library.models.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
+
+    @Query("select a from Author a where a.lastName =?1")
+    List<Author> findAuthorByLastName(String name);
 }
--- src/main/java/library/repositories/BookRepository.java
+++ src/main/java/library/repositories/BookRepository.java
@@ -2,6 +2,21 @@ package library.repositories;
 
 import library.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
+
+    @Query("select b from Book b where b.title=?1")
+    List<Book> findBookByTitle (String title);
+
+    @Query //dopisać querry
+    List<Book> findBookByAuthor(@Param("author") String author);
+
+    @Query //dopisać querry
+    List<Book> findBookByCategory(@Param("category") String category);
 }
