ba6449bf4ee125b4ab4809e920ddcf5e919eeb41
Damian Grochowski <smg7@o2.pl>
2019-05-06 15:09:45 +0200

    x

--- src/main/java/library/repositories/PaymentRepository.java
+++ src/main/java/library/repositories/PaymentRepository.java
@@ -5,6 +5,7 @@ import library.models.Payment;
 import library.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,10 +14,11 @@ import java.util.List;
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
 
 
+
     /* metoda zwraca listę płatnośći powyżej 10zł
     TODO: zamiast 10 umieścić zmienną, w razie potrzeby zmiany wartośći */
-    @Query("select p from Payment  p where p.amount> 10")
-    List<Payment> findByAmount(Integer amount);
+    @Query("select p from Payment  p where p.amount> :value")
+    List<Payment> findByAmount(@Param("value") Integer amount);
 
 
     //metoda zwraca listę płatności danego użytkownika
--- src/main/java/library/validators/ReturnBookVaildator.java
+++ src/main/java/library/validators/ReturnBookVaildator.java
@@ -2,14 +2,28 @@ package library.validators;
 
 import library.models.BookState;
 import library.models.Payment;
+import library.repositories.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.Duration;
 
 public class ReturnBookVaildator {
+
+
+    private PaymentRepository paymentRepository;
+
+    @Autowired
+    public ReturnBookVaildator(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+
     //mnożnik dziennej opłaty (kary) za przetrzymanie książki, obecnie =1zł
     final Integer DAILY_MULTIPLIER = 1;
 
-    //nazwa do zmiany, zmienna odpowiada za wartość kary pieniężnej, po której np. użytownik nie może wypożyczyć następnej książki
+    /**
+     * nazwa do zmiany, zmienna odpowiada za wartość kary pieniężnej,
+     * po której np. użytownik nie może wypożyczyć następnej książki
+     */
     final Integer SOME_PENALTY_AMOUNT = 10;
 
     public void returningBookVaildator(BookState bookState) {
@@ -20,15 +34,21 @@ public class ReturnBookVaildator {
 
         if (days > 30) {
             Payment payment = new Payment();
+            /**
+             * konwersja longa (Duration zwraca w longu) na Integer
+             * (zgodny ze zmienną amount z klasy Payment)
+             */
 
-            //konwersja longa (Duration zwraca w longu) na Integer
-            // (zgodny ze zmienną amount z klasy Payment)
             Integer days2 = days.intValue();
 
-            //naliczanie kary za każdy dzień przetrzymania książki
-            //za każdy dzień powyżej 30 dni, naliczana jest złotówka kary
+            /**
+             * naliczanie kary za każdy dzień przetrzymania książki
+             * za każdy dzień powyżej 30 dni, naliczana jest złotówka kary
+             */
+
             payment.setAmount((days2 - 30) * DAILY_MULTIPLIER);
 
+            paymentRepository.save(payment);
         }
     }
 
