e9f4b5b3f630604a4af2436bed4a7df45c14edb0
Damian Grochowski <smg7@o2.pl>
2019-05-18 00:23:40 +0200

    added @Scheduled annotation with cron, sendMail method improved a little bit

--- src/main/java/library/services/EmailService.java
+++ src/main/java/library/services/EmailService.java
@@ -5,45 +5,60 @@ import javax.mail.internet.MimeMessage;
 
 import library.models.Book;
 import library.repositories.BookRepository;
+import library.repositories.BookStateRepository;
 import library.repositories.UserRepository;
 import library.users.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
+@Slf4j
 @Service
+@EnableScheduling
 public class EmailService {
     private RentService rentService;
     private JavaMailSender javaMailSender;
     private BookRepository bookRepository;
     private UserRepository userRepository;
+    private BookStateRepository bookStateRepository;
 
     @Autowired
-    public EmailService(RentService rentService, JavaMailSender javaMailSender, BookRepository bookRepository, UserRepository userRepository) {
+    public EmailService(RentService rentService, JavaMailSender javaMailSender, BookRepository bookRepository, UserRepository userRepository, BookStateRepository bookStateRepository) {
         this.rentService = rentService;
         this.javaMailSender = javaMailSender;
         this.bookRepository = bookRepository;
         this.userRepository = userRepository;
+        this.bookStateRepository = bookStateRepository;
     }
 
     /**
-     * wysyłanie maili
+     * wysyłanie maili, póki co dla konkretnego użytkownika
      * //:TODO do poprawienia/zrobić wariant dla różnych akcji
-     */
-    public void send(String email, String title, Integer bookId, Integer userId) {
-        MimeMessage mail = javaMailSender.createMimeMessage();
-        try {
-            MimeMessageHelper helper = new MimeMessageHelper(mail, true);
-            helper.setTo(email);
-            helper.setReplyTo("@przykladowy@mail.com");         //:TODO : ustawić maila
-            helper.setFrom("@przykladowy@mail.com");            //:TODO : ustawić maila
-            helper.setSubject(title);
-            helper.setText(rentService.rentBook(bookRepository.getOne(bookId), userRepository.getOne(userId)));
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        } finally { //:TODO ?
+     */ //:TODO do przerobienia : metoda powinna przeszukać całą bazę w poszukiwaniu wszystkich aktualnych wypożyczeń i po nich sprawdzać daty oddania-w razie spełnienia warunków należy wysłać maila
+    @Scheduled(cron = "0 0 21 * * *") //cron-w tej konfiguracji ustawia wykonanie zadania codziennie o godzinie 21
+    public void send(String title, Integer bookId, Integer userId) {
+        if (bookStateRepository.findBookStateByBook(bookId).getDateOfReturn().equals(LocalDate.now().plusDays(30))) {
+            MimeMessage mail = javaMailSender.createMimeMessage();
+            try {
+                MimeMessageHelper helper = new MimeMessageHelper(mail, true);
+                helper.setTo(userRepository.getOne(userId).getEmail());
+                helper.setReplyTo("@przykladowy@mail.com");         //:TODO : ustawić maila
+                helper.setFrom("@przykladowy@mail.com");            //:TODO : ustawić maila
+                helper.setSubject(title);
+                helper.setText(rentService.rentBook(bookRepository.getOne(bookId), userRepository.getOne(userId)));
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            } finally { //:TODO ?
+            }
+            javaMailSender.send(mail);
+        } else {
+            log.info("Nie wysłano żadnej wiadomości"); //
         }
-        javaMailSender.send(mail);
     }
 }
